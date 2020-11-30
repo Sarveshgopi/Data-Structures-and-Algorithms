d276b6fe7dbaebae38ef704f9f32401d4e965cbf
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-11-30 14:04:10 +0530

    Create Arithmetic Progression.c

---
+++ Mathematical/Arithmetic Progression.c
@@ -0,0 +1,20 @@
+/*   ALGORITHM:
+               The nth term in the arithmetic progression is firstterm + (n-1)*commondifference
+               The sum of first n numbers is (0.5*n) + (2*firstterm)+(n-1)*commondifference             */
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{  
+   int firstterm,secondterm;
+   scanf("%d %d",&firstterm,&secondterm);
+   int commondiff=secondterm-firstterm;
+   int n;
+   scanf("%d",&n);
+   int nterm = firstterm + ((n-1)*commondiff);
+   int sum_of_nterm = (0.5)*n + ((2*firstterm)+((n-1)*commondiff));
+   printf("The nth term is:\n");
+   printf("%d",nterm);
+   printf("The sum of first n terms is:\n");
+   printf("%d",sum_of_nterm);
+   return 0;
+}
