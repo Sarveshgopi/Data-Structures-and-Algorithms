65e4c4991186aeacade7129a49b794b573cd03b6
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-12-10 23:32:57 +0530

    Create Creation of Singly Linked List.cpp

---
+++ Linked List/Creation of Singly Linked List.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct LL
+{
+    int data;
+    struct LL *next;
+ };
+ 
+ void create(int a[],int n)
+ {
+     int i;
+     struct LL *first=NULL;
+     for(i=0;i<n;i++)
+ }
+int main()
+{
+      
