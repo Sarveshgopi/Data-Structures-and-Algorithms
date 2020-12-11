80be5bf0c1381c4962cf96de097a32cc37e8ed0e
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-12-11 22:39:43 +0530

    Update Mean.c

--- Mathematical/Mean.c
+++ Mathematical/Mean.c
@@ -5,11 +5,21 @@ int main()
      int n;
      scanf("%d",&n);
      int a[n],i,sum=0;
+     float k;
      for(i=0;i<n;i++)
      {
          scanf("%d ",&a[i]);
          sum+=a[i];
      }
-     printf("The Mean is : %d",(int)sum/n);
+     if(n%2!=0)
+     {
+          k=a[n/2];
+     }
+     else
+     {
+          k=(a[n/2]+a[(n/2)-1])/2;
+     }
+     printf("The Mean is : %d\n",(int)sum/n);
+     printf("The Median is : %d",k);
      return 0;
  }
