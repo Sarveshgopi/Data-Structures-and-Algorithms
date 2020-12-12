8374f406fa43b15d2b596ebbca56adbbe39a53d9
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-12-12 23:17:29 +0530

    Create Digits frequency.c

---
+++ Strings/Digits frequency.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+     char a[1001];
+     scanf("%s",a);
+     int i,j,c;
+     for(i=48;i<=57;i++)
+     {
+         c=0;
+         for(j=0;j<strlen(a);j++)
+         {
+             if(a[j]==i)
+             {
+                c++;
+              }
+          }
+        printf("%d ",c);
+    }
+    return 0;
+    }
