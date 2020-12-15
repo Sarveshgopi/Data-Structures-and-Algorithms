fcfcf640e8861d229f573eb4089e2746e64f1cb2
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2020-12-15 13:38:06 +0530

    Update Creation and Display Singly Linked List.c

--- Linked List/Creation and Display Singly Linked List.c
+++ Linked List/Creation and Display Singly Linked List.c
@@ -5,22 +5,43 @@ struct LL
 {
     int data;
     struct LL *next;
- };
+ }*first=NULL,*last=NULL;
  
  void create(int a[],int n)
  {
      int i;
-     struct LL *first=NULL;
-     for(i=0;i<n;i++)
+     struct LL *t;
+     first=(struct LL*)malloc(sizeof(struct LL));
+     first->data=a[0];
+     first->next=NULL;
+     last=first;
+     for(i=1;i<n;i++)
+     { 
+        t=(struct LL*)malloc(sizeof(struct LL));
+        t->data=a[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+     }
  }
+
+void display(struct LL *p)
+{
+    while(p)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+}
+
 int main()
 {
       int n;
       scanf("%d",&n);
-      int a[n],i;
-      for(i=0;i<n;i++)
+      int a[n],z;
+      for(z=0;z<n;z++)
       {
-          scanf("%d ",&a[i]);
+          scanf("%d ",&a[z]);
       }
     create(a,n);
     display(first);
