84de3d939d75719d61a981677492f3f4d66b8327
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-02-01 00:25:50 +0530

    Create Merging Two SinglyLL.c

---
+++ Linked List/Merging Two SinglyLL.c
@@ -0,0 +1,55 @@
+/* This is a code snippet */
+ struct LL
+ {
+    int data;
+    struct LL *next;
+ };
+ 
+ // Merging means concantenate them in a sorted manner.
+ 
+ struct LL *merge(struct LL* first, struct LL* second)
+ {
+    struct LL *p=first,*q=second,*third,*last;
+    if(p->data < q->data)
+    {
+         third=last=p;
+         p=p->next;
+         last->next=NULL;
+    }
+    else
+    {
+         third=last=q;
+         q=q->next;
+         last->next=NULL;
+    }
+    while(p && q)
+    {
+        if(p->data < q->data)
+        {
+             last=p;
+             p=p->next;
+             last->next=NULL;
+        }
+        else
+        {
+            last=q;
+            q=q->next;
+            last->next=NULL;
+        }
+    }
+    if(p!=NULL)
+    {
+       last->next=p;
+     }
+     else
+     {
+        last->next=q;
+      }
+      return third;
+  }
+  
+  /*
+  Analysis:
+       Time efficiency ϵ θ(m+n)
+       m is the no. of nodes in 1st LL
+       n is the no. of nodes in 2nd LL
