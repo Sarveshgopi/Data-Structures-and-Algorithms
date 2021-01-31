a2443fe6a311a3100013fcc759d26d2eefafae12
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-01-31 23:41:46 +0530

    Create Detect Loop in SinglyLL.c

---
+++ Linked List/Detect Loop in SinglyLL.c
@@ -0,0 +1,25 @@
+/* This is a code snippet */
+
+struct LL
+{
+    int data;
+    struct LL *next;
+};
+
+//Detecting loop using circular move algorithm
+struct LL* detect_loop(struct LL *first)
+{
+     struct LL *p,*q;
+     p=q=first;
+     do
+     {
+         p=p->next;
+         q=q->next;
+         q=(q!=NULL)?(q->next):(NULL);
+     }while(p!=NULL && q!=NULL && p!=q)
+     if(p==q)
+     {
+       return true;
+     }
+     return false;
+ }
