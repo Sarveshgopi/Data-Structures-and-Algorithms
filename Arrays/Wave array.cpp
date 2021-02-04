89b34145323cabaa565d7f5304ad49525eb4cb47
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-02-04 14:06:20 +0530

    Create Wave array.cpp

---
+++ Arrays/Wave array.cpp
@@ -0,0 +1,17 @@
+/* This is just a code snippet   */
+
+class solution
+{
+    public:
+        void wave_array(int *arr,int size)
+        {
+            int index,temp;
+            for(index=0;index<(size-1);index+=2)
+            {
+                  temp=arr[i];
+                  arr[i]=arr[i+1];
+                  arr[i+1]=temp;
+            }
+       }
+   };
+        
