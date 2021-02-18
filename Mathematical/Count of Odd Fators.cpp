027d8e91b2d85093e66ee42a492938b3e50e2737
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-02-18 12:22:51 +0530

    Create Count of Odd Fators.cpp

---
+++ Mathematical/Count of Odd Fators.cpp
@@ -0,0 +1,12 @@
+/* This is just a cod snippet  */
+
+
+/* Find the numbers in the range from 1 to N having odd count of fators  */
+class Solution
+{
+   public:
+   int odd_count_fact(int N)
+   {
+       return pow(N,0.5);
+    }
+};
