6a63592ffe5113de055a049fffe3c0ec10af144c
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-03-05 23:40:56 +0530

    Update Preorder Traversal.cpp

--- Trees/Preorder Traversal.cpp
+++ Trees/Preorder Traversal.cpp
@@ -1,1 +1,21 @@
+/* This is just a code snippet  */
 
+/* Preorder -> root , left , right  */
+
+struct tree
+{
+  int data;
+  struct tree *left,*right;
+}*root=NULL;
+
+
+void postorder(struct tree *root)
+{
+  if(root==NULL)
+  {
+    return ;
+  }
+  std::cout<<root->data<<" ";
+  postorder(root->left);
+  postorder(root->right);
+}
