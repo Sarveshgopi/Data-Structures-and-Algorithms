2b1cdb55a9791ef361c3438c9c16afd25d8975ff
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-04-22 22:51:20 +0530

    Update Count of overlapping substring.cpp

--- Strings/Count of overlapping substring.cpp
+++ Strings/Count of overlapping substring.cpp
@@ -8,3 +8,18 @@ int main(int argc, char **argv)
      cout.tie(0);
      std::string input_str;
      std::cin>>input_str;
+     std::string key;
+     std::cin>>key;
+     int count=0,start=0;
+     while(count<input_str.size())
+     {
+          size_t pos=input_str.find(key,start);
+          if(pos!=string::npos)
+          {
+               start=pos+1;
+               count+=1;
+          }
+          else
+          {
+               break;
+          }
