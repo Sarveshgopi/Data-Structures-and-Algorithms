8af6643088f6328969503ede316ed5dc85ebeb47
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-04-22 22:47:03 +0530

    Create Count of overlapping substring.cpp

---
+++ Strings/Count of overlapping substring.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+     ios_base::sync_with_stdio(false);
+     cin.tie(0);
+     cout.tie(0);
+     std::string input_str;
+     std::cin>>input_str;
