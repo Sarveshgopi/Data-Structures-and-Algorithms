97d569237c7aeb3e67d3ba6118bb785f7174ab67
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-04-28 00:02:49 +0530

    Update Odd or Even.c

--- Bit Manipulation/Odd or Even.c
+++ Bit Manipulation/Odd or Even.c
@@ -3,5 +3,15 @@
 
 int main()
 {
+      int x;
+      scanf("%d",&x);
+      if(!(x&1))
+      {
+            printf("Even");
+      }
+      else
+      {
+            printf("Odd");
+      }
       return 0;
  }
