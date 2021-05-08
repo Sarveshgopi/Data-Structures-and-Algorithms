3e87e9df0ca4973efb73944e0235672af0e200e3
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-05-09 00:28:20 +0530

    Create Level order traversal.cpp

---
+++ Trees/Level order traversal.cpp
@@ -0,0 +1,27 @@
+/*  This is just a code snippet  */
+
+class Solution
+{
+  public:
+       void getLevelDiff(struct node *root)
+       {
+         std::queue<struct node*> a;
+         a.push(root);
+         struct node *p=root;
+         std::cout<<"The level order traversal is : "<<endl;
+         while(p!=NULL)
+         {
+             std::cout<<p->data<<" ";
+             if(p->left!=NULL)
+             {
+                 a.push(p->left);
+             }
+             if(p->right!=NULL)
+             {
+                 a.push(p->right);
+             }
+             a.pop();
+             p=a.front();
+         }
+    }
+};
