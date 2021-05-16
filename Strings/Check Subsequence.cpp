257fa64b4300afa2d73cd290a5671ee11f772746
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-05-16 23:53:45 +0530

    Create Check Subsequence.cpp

---
+++ Strings/Check Subsequence.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(int *argv,char **argc)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    std::string main,sub;
+    std::cin>>main;
+    std::cin>>sub;
