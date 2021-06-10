7637ed7cf456925a9c6ac0b18cd6575f340c26d7
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-06-11 00:46:51 +0530

    Update Height of a binary tree.c

--- Trees/Height of a binary tree.c
+++ Trees/Height of a binary tree.c
@@ -6,3 +6,25 @@ struct tree
     struct tree *left;
     struct tree *right;
  }*root=NULL;
+
+
+int height(struct tree *root)
+{
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        int l=height(root->left);
+        int r=height(root->right);
+        if(l>=r)
+        {
+            return l+1;
+        }
+        else
+        {
+            return r+1;
+        }
+    }
+}
