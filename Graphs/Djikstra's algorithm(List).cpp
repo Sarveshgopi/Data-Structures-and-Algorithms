8b60e55077995a6ec2cb06cd67b3f3bf42ad03ac
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-06-19 16:29:51 +0530

    Update and rename Djikstra's algorithm.cpp to Djikstra's algorithm(List).cpp

---
+++ Graphs/Djikstra's algorithm(List).cpp
@@ -0,0 +1,29 @@
+/*     For Adjacency List     */
+
+class Solution
+{
+	public:
+  vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        std::vector<int> out(V,INT_MAX);
+        out[S]=0;
+        std::vector<bool> vis(V,false);
+        std::priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > a;
+        a.push(make_pair(S,0));
+        while(!a.empty())
+        {
+            int p=a.top().first;
+            a.pop();
+            vis[p]=true;
+            for(auto i:adj[p])
+            {
+                if(out[p]+i[1]<out[i[0]])
+                {
+                    out[i[0]]=out[p]+i[1];
+                    a.push(make_pair(i[0],out[i[0]]));
+                }
+            }
+        }
+        return out;
+    }
+};
