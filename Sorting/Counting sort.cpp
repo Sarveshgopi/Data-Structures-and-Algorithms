3045dc990548185a710addc941024b5e6db79405
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-07-17 23:52:17 +0530

    Update Counting sort.cpp

--- Sorting/Counting sort.cpp
+++ Sorting/Counting sort.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+     ios_base::sync_with_stdio(0);
+     cin.tie(0);
+     cout.tie(0);
+     std::string a;
+     std::cin>>a;
+     int out[26]={0},i,j;
+     for(i=0;i<a;i++)
+     {
+       out[a[i]-97]++;
+     }
+     for(i=0;i<26;i++)
+     {
+       for(j=0;j<out[i];j++)
+       {
+            printf("%c",i+97);
+       }
+     }
+  return 0;
+}
