083171060106b5003eafbf4ae144224e8d4055fa
Sarveshgopi <55702877+Sarveshgopi@users.noreply.github.com>
2021-08-10 20:51:13 +0530

    Update Merge sort.cpp

--- Sorting/Merge sort.cpp
+++ Sorting/Merge sort.cpp
@@ -38,5 +38,6 @@ void mergesort(int arr[],int left,int right)
      int mid=left+(right-left)/2;
      mergesort(arr,left,mid);
      mergesort(arr,mid+1,right);
+     merge(arr,left,mid,right);
   }
 }
